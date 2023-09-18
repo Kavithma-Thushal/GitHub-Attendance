609382f192c80949f2215c2c57f703d40eb3393c
Kavithma Thushal <125787087+Thushal2001@users.noreply.github.com>
2023-09-18 18:15:17 +0530

    Commit A

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+/**
+ * @author : Kavithma Thushal
+ * @project : Github-Attendance
+ * @since : 6:14 PM - 9/18/2023
+ **/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("A");
+    }
+}
