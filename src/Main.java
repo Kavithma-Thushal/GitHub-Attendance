84e06f41214cb9dc23ef01ffd7d4787024439447
Kavithma Thushal <125787087+Thushal2001@users.noreply.github.com>
2023-10-03 07:48:56 +0530

    Commit

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,20 @@
  **/
 public class Main {
     public static void main(String[] args) {
+        System.out.println("1");
+        System.out.println("2");
+        System.out.println("3");
+        System.out.println("4");
+        System.out.println("5");
+        System.out.println("6");
+        System.out.println("7");
+        System.out.println("8");
+        System.out.println("9");
+        System.out.println("10");
+        System.out.println("11");
+        System.out.println("12");
+        System.out.println("13");
+        System.out.println("14");
+        System.out.println("15");
     }
 }
