e4e3d18528a619319609c4f9d25a58cd5ac04345
Kavithma Thushal <125787087+Thushal2001@users.noreply.github.com>
2023-11-11 07:00:14 +0530

    Commit

--- src/Main.java
+++ src/Main.java
@@ -18,5 +18,13 @@ public class Main {
         System.out.println("11");
         System.out.println("12");
         System.out.println("13");
+        System.out.println("14");
+        System.out.println("15");
+        System.out.println("16");
+        System.out.println("17");
+        System.out.println("18");
+        System.out.println("19");
+        System.out.println("20");
+        System.out.println("21");
     }
 }
